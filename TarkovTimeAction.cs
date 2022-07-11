757363ed48d1073f4d78d80edeb2f48d9af10799
fmjensen <flemming@munkjensen.net>
2022-07-11 11:39:45 +0200

    Plugin updates Tarkov Time on it's own now. Updates are done every 7 real-time seconds since one Tarkov minute last ~8.6 real-time seconds. This mean that the display can be up to 7 seconds off. I am certain that will not have any impact on the plugin functionality and usefullness.

--- TarkovTimeAction.cs
+++ TarkovTimeAction.cs
@@ -2,6 +2,7 @@
 using StreamDeckLib;
 using Newtonsoft.Json;
 using System.Net.Http;
+using System.Threading;
 using StreamDeckLib.Messages;
 using System.Threading.Tasks;
 
@@ -16,36 +17,31 @@ namespace TarkovTime
         {
             // Clear the stored Tarkov Time.
             SettingsModel.JsonData = "";
-
             // Update the PluginTitle with the empty string
             await Manager.SetTitleAsync(args.context, SettingsModel.JsonData.ToString());
-
             // Save the empty Tarkov Time string.
             await Manager.SetSettingsAsync(args.context, SettingsModel.JsonData.ToString());
         }
 
         public override async Task OnKeyUp(StreamDeckEventPayload args)
         {
-            // Fetch the current Tarkov Times
-            var TTleft = tt.RealTimeToTarkovTime(true);
-            var TTright = tt.RealTimeToTarkovTime(false);
-            // Combine the two the current Tarkov Times and store it in SettingsModel
-            SettingsModel.JsonData = TTleft + "\n" + TTright;
-            // Update the PluginTitle with the current Tarkov Times
-            await Manager.SetTitleAsync(args.context, SettingsModel.JsonData.ToString());
-            // Save the updated TArkov Times
-            await Manager.SetSettingsAsync(args.context, SettingsModel.JsonData.ToString());
+            await UpdateTTAsync(args);
         }
 
         public override async Task OnDidReceiveSettings(StreamDeckEventPayload args)
         {
             await base.OnDidReceiveSettings(args);
-            await Manager.SetTitleAsync(args.context, SettingsModel.JsonData.ToString());
         }
 
         public override async Task OnWillAppear(StreamDeckEventPayload args)
         {
             await base.OnWillAppear(args);
+            // Start timer her måske??
+            await Run(args, TimeSpan.FromSeconds(7), CancellationToken.None);
+        }
+
+        public async Task UpdateTTAsync(StreamDeckEventPayload args)
+        {
             // Fetch the current Tarkov Times
             var TTleft = tt.RealTimeToTarkovTime(true);
             var TTright = tt.RealTimeToTarkovTime(false);
@@ -55,6 +51,17 @@ namespace TarkovTime
             await Manager.SetTitleAsync(args.context, SettingsModel.JsonData.ToString());
             // Save the updated TArkov Times
             await Manager.SetSettingsAsync(args.context, SettingsModel.JsonData.ToString());
+        }
+
+        public async Task Run(StreamDeckEventPayload args, TimeSpan period, CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(period, cancellationToken);
+
+                if (!cancellationToken.IsCancellationRequested)
+                    await UpdateTTAsync(args);
+            }
         }
     }
 }
